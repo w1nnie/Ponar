c8c0f616eb4d0211ccc45f4adb771728f354932d
winnie <winnie.trap@gmail.com>
2019-07-06 01:56:51 +0900

    adjust start effect and add OnTrackMove

---
+++ Assets/OnTrackMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTrackMove : MonoBehaviour
+{
+    float direction = 40.0f;
+    float speed = 5.0f;
+    Vector2 v;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        v.x = 0;
+        v.y = 1;
+
+        Rigidbody2D rd = GetComponent<Rigidbody2D>();
+        rd.velocity = v;
+    }
+}
