3a207a4cedcb59e64da7b93363fd5fcc19ec0d86
winnie <winnie.trap@gmail.com>
2019-07-06 01:25:32 +0900

    emit particle ontrack

---
+++ Assets/CustomDefaultTrackableEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Vuforia;
+
+public class CustomDefaultTrackableEventHandler : DefaultTrackableEventHandler
+{
+    public UnityEvent OnTrackingAction;
+    public UnityEvent OffTrackingAction;
+
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+        OnTrackingAction.Invoke();
+    }
+
+    protected override void OnTrackingLost()
+    {
+        base.OnTrackingLost();
+        OffTrackingAction.Invoke();
+    }
+}
