5b4d49baed05400e7625d042731b9fea21aefe02
winnie <winnie.trap@gmail.com>
2019-07-06 14:40:26 +0900

    improve emmergence of woofar

--- Assets/OnTrackMove.cs
+++ Assets/OnTrackMove.cs
@@ -2,20 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class OnTrackMove : MonoBehaviour
 {
-    float direction = 40.0f;
-    float speed = 5.0f;
-    Vector2 v;
+    float scale = 0.003f;
+    float time;
+    float alpha;
+    float alphaControlTime = 0.7f;
+    float alphaControlCoefficient = 7f;
+    SpriteRenderer image;
+
+    void Start()
+    {
+        time = 0;
+        alpha = 0.01f;
+        image = GetComponent<SpriteRenderer>();
+    }
 
     // Update is called once per frame
     void Update()
     {
+        time += 0.02f;
+        float dy = Mathf.Sin(2 * Mathf.PI * 0.7f * time) * scale;
+        this.transform.position += new Vector3(0,dy,0);
+        Debug.Log(time);
+        FadeIn();
+        
+    }
 
-        v.x = 0;
-        v.y = 1;
+    void FadeIn()
+    {
+        alpha += Time.deltaTime * alphaControlTime * alpha * alphaControlCoefficient;
+        if (alpha >= 1f)
+        {
+            alpha = 1f;
+        }
+        var color = image.color;
+        color.a = alpha;
+        image.color = color;
 
-        Rigidbody2D rd = GetComponent<Rigidbody2D>();
-        rd.velocity = v;
     }
 }
