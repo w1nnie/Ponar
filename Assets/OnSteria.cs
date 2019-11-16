784e3926490d0b7bf57abdf11cad133580b63145
winnie <winnie.trap@gmail.com>
2019-11-16 18:29:51 +0900

    return environment and pixelize vfx

---
+++ Assets/OnSteria.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.VFX;
+using UnityEngine;
+
+
+public class OnSteria : MonoBehaviour
+{
+    GameObject steria;
+    float spawnRate;
+    public bool isOnSteria;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnRate = 0;
+        // diffusion.SpawnRate = spawnRate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    public void SetParameter(float value)
+    {
+        VisualEffect vfx = steria.GetComponent<VisualEffect>();
+        vfx.SetFloat("paramName", value);
+    }
+}
