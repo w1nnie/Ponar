def23da8ea4f18cf1a96a0b90469d43f9292f4d7
winnie <winnie.trap@gmail.com>
2019-11-17 00:00:26 +0900

    repeat onTrackSteria

--- Assets/Scripts/OnSteria.cs
+++ Assets/Scripts/OnSteria.cs
@@ -3,27 +3,50 @@ using System.Collections.Generic;
 using UnityEngine.VFX;
 using UnityEngine;
 
-
 public class OnSteria : MonoBehaviour
 {
-    GameObject steria;
-    VisualEffect pixelize;
-    float spawnRate;
+    GameObject pixelize;
+    float time;
+    float sr;
     public bool isOnSteria;
-    // Start is called before the first frame update
     void Start()
     {
-        spawnRate = 0;
-        SetParameter(spawnRate);
+        time = 0f;
+        isOnSteria = false;
+        SetParameter(0);
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (!isOnSteria)
+        {
+            time = 0f;
+        }
+        else
+        {
+            time += Time.deltaTime;
+            Debug.Log(isOnSteria);
+            if(time > 3 && time < 5.5)
+            {
+                SetParameter((float)(1000*(5.5-time)));
+            }
+            else if(time >= 3.5)
+            {
+                SetParameter(0);
+            }
+        }
     }
     public void SetParameter(float value)
     {
-        VisualEffect vfx = pixelize.GetComponent<VisualEffect>();
-        vfx.SetFloat("swawnRate", value);
+        VisualEffect vfx = GetComponent<VisualEffect>();
+        vfx.SetFloat("spawnRate", value);
+    }
+    public void trackOnSteria()
+    {
+        isOnSteria = true;
+    }
+    public void trackOffSteria()
+    {
+        isOnSteria = false;
     }
 }
