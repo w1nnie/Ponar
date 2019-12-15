4e6e77741bbe8030c79a4108f33b7f7b42a2d9ed
winnie <winnie.trap@gmail.com>
2019-12-16 07:05:10 +0900

    apply trackTime

---
+++ Assets/Scripts/OnSteria_2.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.VFX;
+using UnityEngine;
+
+public class OnSteria_2 : MonoBehaviour
+{
+    GameObject steria;
+    float time;
+    float alpha;
+    float sr;
+    public bool isOnSteria;
+    void Start()
+    {
+        time = 0f;
+        isOnSteria = false;
+        SetVFXParameter(0,0);
+        SetVFXParameter(1,0);
+    }
+
+    void Update()
+    {
+        if (!isOnSteria)
+        {
+            time = 0f;
+            SetSpriteAlpha(0,0);
+            SetSpriteAlpha(1,0);
+            SetVFXParameter(0,0);
+        }
+        else
+        {
+            time += Time.deltaTime;
+            textAnimation(time);
+            pixelizeAnimation(time);
+            pixelartAnimation(time);
+            detectAnimation(time);
+        }
+    }
+    void textAnimation(float time)
+    {
+        if(time < 2.8)
+        {
+            float f = Mathf.Sin((float)(2 * Mathf.PI * time / (2.8 * 3) * 4));
+            SetSpriteAlpha(1, Mathf.Abs(f));
+        }
+        else if(time > 2.8 && time < 3)
+        {
+            SetSpriteAlpha(1,1);
+        }
+        else if(time > 3)
+        {
+            SetSpriteAlpha(1,0);
+        }
+    }
+    void pixelizeAnimation(float time)
+    {
+        if(time < 5){
+            SetVFXParameter(0,time);
+        }
+    }
+    void pixelartAnimation(float time)
+    {
+        if(time > 3 && time < 4.8)
+        {
+            pixelartFadeIn(0);
+        }
+    }
+    void detectAnimation(float time)
+    {
+        if(time < 5)
+        {
+            SetVFXParameter(1,time);
+        }
+    }
+    void pixelartFadeIn(int index)
+    {
+        double a = (time - 3) / 1.8;
+        SetSpriteAlpha(index, (float)a);
+    }
+    void SetSpriteAlpha(int index, float alpha)
+    {
+        SpriteRenderer srn = GetComponentsInChildren<SpriteRenderer>()[index];
+        var color = srn.color;
+        color.a = alpha;
+        srn.color = color;
+    }
+    void SetVFXParameter(int index,float value)
+    {
+        VisualEffect vfx = GetComponentsInChildren<VisualEffect>()[index];
+        vfx.SetFloat("onTrackTime", value);
+    }
+    public void trackOnSteria()
+    {
+        isOnSteria = true;
+    }
+    public void trackOffSteria()
+    {
+        isOnSteria = false;
+    }
+}
